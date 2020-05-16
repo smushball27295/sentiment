e0608c07efefe5763277b37eac2bcb5a49668dfb
ivanbakrac <48951541+ivanbakrac@users.noreply.github.com>
2020-05-16 13:45:52 -0400

    Update classifier_final.py

--- selenium/classifier_final.py
+++ selenium/classifier_final.py
@@ -23,19 +23,19 @@ from sklearn.model_selection import cross_val_score
 from nltk.stem.porter import PorterStemmer
 
 start_time = time.time()
+stop = stopwords.words("english")
+
+tfidf = TfidfVectorizer()
+cvec = CountVectorizer()
 
 # reference: Python Machnine Learning by Sebastian Raschika
 # ===========================================================================================
 def tokenizer(text):
     return text.split()
-
-
 # ===========================================================================================
 def tokenzier_porter(text):
     porter = PorterStemmer()
     return [porter.stem(words) for words in text.split()]
-
-
 # ===========================================================================================
 def load_label_data(fname):
     actname = []
@@ -54,8 +54,6 @@ def load_label_data(fname):
 
     f.close()
     return actname, reviews, date, labels
-
-
 # ==================================================================================================
 def balance_labels(file):
 
@@ -77,16 +75,6 @@ def balance_labels(file):
         )
 
     return rev_train, labels_train, rev_test, labels_test
-
-
-# ==================================================================================================
-print(stats.itemfreq(labels_train))
-print(stats.itemfreq(labels_test))
-
-stop = stopwords.words("english")
-
-tfidf = TfidfVectorizer()
-cvec = CountVectorizer()
 # ==================================================================================================
 # LREG
 # ==================================================================================================
@@ -126,8 +114,6 @@ def lreg(rev_train, labels_train, rev_test, labels_test):
     predictedLREG_BEST = lreg_clfgs_best.predict(rev_test)
 
     return predictedLREG_BEST
-
-
 # ==================================================================================================
 # NB
 # ==================================================================================================
@@ -164,8 +150,6 @@ def nb(rev_train, labels_train, rev_test, labels_test):
     predictedNB_BEST = gridsearch_nb_best.predict(rev_test)
 
     return predictedNB_BEST
-
-
 # ==================================================================================================
 if __name__ == "__main__":
 
