b7b84dbcb7dd1c54cb65b85191e5041d0573563a
Anirudh Raghavan Sundararaghavan <54192291+Ani-07@users.noreply.github.com>
2020-05-15 23:30:25 -0400

    Update and rename flask_server.py to aws_server.py

--- flask/aws_server.py
+++ flask/aws_server.py
@@ -6,7 +6,7 @@ Created on Tue May 12 15:20:56 2020
 """
 import os
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path = "/static", static_folder = "static")
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
@@ -16,11 +16,10 @@ from Econ_Public_Sent_Static import post_percent, neg_percent,last_up,tweets_ana
 
 @app.route('/Twitter_Data_Analysis', methods=['GET'])
 def sent_results():
-    return render_template('595_Template.html', 
+    return render_template('FE595_flask_template_aws.html', 
                            twit_sent1 = post_percent, twit_sent2 = neg_percent, 
                            time = last_up, tweets = tweets_analyzed) 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
-
 
